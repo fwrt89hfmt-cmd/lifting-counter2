0c45fc30f2d35da4dccd720cdd0559548211150d
fwrt89hfmt-cmd <fwrt89hfmt@privaterelay.appleid.com>
2026-09-12 19:16:09 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('fetch',e=>{});
+const C='lifting-v2';self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'])));self.skipWaiting()});self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
